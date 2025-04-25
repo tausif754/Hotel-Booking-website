007fa2b11ec60aa421d7bb988cf096b0e9f606aa
Tausif Ahmad <tausifahmad754@gmail.com>
2025-04-25 12:32:41 +0530

    Hero background image added

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,20 @@
 import React from "react";
 import Navbar from "./components/Navbar";
-import { useLocation } from "react-router-dom";
+import { Routes, Route, useLocation } from "react-router-dom";
+import Home from "./pages/Home";
 
 const App = () => {
   const isOwnerPath = useLocation().pathname.includes("/owner");
-  return <div>{!isOwnerPath && <Navbar />}</div>;
+  return (
+    <div>
+      {!isOwnerPath && <Navbar />}
+      <div className="min-h-[70vh]">
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </div>
+    </div>
+  );
 };
 
 export default App;
