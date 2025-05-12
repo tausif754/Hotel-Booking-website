69928d32ed358f0a5f9804633cdd78868105caf9
Tausif Ahmad <tausifahmad754@gmail.com>
2025-05-12 23:51:50 +0530

    My booking page added

---
+++ src/pages/MyBookings.jsx
@@ -0,0 +1,59 @@
+import React from "react";
+import Title from "../components/Title";
+import { assets, userBookingsDummyData } from "../assets/assets";
+import { useState } from "react";
+
+const MyBookings = () => {
+  const [bookings, setBookings] = useState(userBookingsDummyData);
+  return (
+    <div className="py-28 md:pb-35 md:pt-32 px-4 md:px-16 lg:px-24 xl:px-32">
+      <Title
+        title="My Bookings"
+        subTitle="Easily manage your past, current, and upcomming hotel reservations in one place. Plan your trips seamlessly with just a few clicks"
+        align="left"
+      />
+
+      <div className="max-w-6xl mt--8 w-full text-gray-800">
+        <div className="hidden md:grid md:grid-cols-[3fr_2fr_1fr] w-full border-b border-gray-300 font-medium text-base py-3">
+          <div className="w-1/3">Hotels</div>
+          <div className="w-1/3">Date & Timings</div>
+          <div className="w-1/3">Payment</div>
+        </div>
+
+        {bookings.map((booking) => (
+          <div
+            key={booking._id}
+            className="grid grid-cols-1 md:grid-cols-[3fr_2fr_1fr] w-full border-b broder-gray-300 py-6 first:border-t"
+          >
+            {/* Hotel details */}
+            <div>
+              <img
+                src={booking.room.images[0]}
+                alt="hotel-img"
+                className="min-md:w-44 rounded shadow object-cover"
+              />
+              <div className="flex flex-col gap-1.5 max-md:mt-3 min-md:ml-4">
+                <p className="font-playfair text-2xl">
+                  {booking.hotel.name}
+                  <span className="font-inter text-sm">
+                    ({booking.room.roomType})
+                  </span>
+                </p>
+                <div className="flex items-center gap-1 text-sm text-gray-500">
+                  <img src={assets.locationIcon} alt="location-icon" />
+                  <div>{booking.hotel.address}</div>
+                </div>
+                {/* Date & Timings */}
+                <div></div>
+                {/* Payment status */}
+                <div></div>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default MyBookings;
